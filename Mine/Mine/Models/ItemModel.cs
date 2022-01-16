ae813ece62b313bfbc9688810fd10bafe550fc0c
Kp1Git <krutaprabhu96@gmail.com>
2022-01-16 15:02:39 -0800

    Added Value attribute to ItemModel.cs Added Value attribute to ItemModel

--- Mine/Mine/Models/ItemModel.cs
+++ Mine/Mine/Models/ItemModel.cs
@@ -7,5 +7,9 @@ namespace Mine.Models
         public string Id { get; set; }
         public string Text { get; set; }
         public string Description { get; set; }
+
+        //The value of Item +9 damage
+        public int value { get; set; } = 0;
+
     }
 }
