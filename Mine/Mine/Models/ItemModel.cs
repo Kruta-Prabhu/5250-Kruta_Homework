80c18b3d1e1ece5b1f3ef2058d7b7c48db469868
Kp1Git <krutaprabhu96@gmail.com>
2022-01-16 15:07:39 -0800

    Added Comments to ItemModel.cs

--- Mine/Mine/Models/ItemModel.cs
+++ Mine/Mine/Models/ItemModel.cs
@@ -2,10 +2,18 @@
 
 namespace Mine.Models
 {
+    /// <summary>
+    /// Items for characters and monsters to use
+    /// </summary>
     public class ItemModel
     {
+        //Id for the Item
         public string Id { get; set; }
+
+        //The display text For the Item
         public string Text { get; set; }
+
+        //The description for the item
         public string Description { get; set; }
 
         //The value of Item +9 damage
