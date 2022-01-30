fbb038e814f1d5af283164a1506b4b1c520aa175
Kruta-Prabhu <krutaprabhu96@gmail.com>
2022-01-29 18:50:14 -0800

    Updated DatabaseFileName to mine in Constants.cs

--- Mine/Mine/Constants.cs
+++ Mine/Mine/Constants.cs
@@ -8,7 +8,10 @@ namespace Mine.ViewModels
 {
     public static class Constants
     {
-        public const string DatabaseFilename = "TodoSQLite.db3";
+        //public const string DatabaseFilename = "TodoSQLite.db3";
+        public const string DatabaseFilename = "mine.db3";
+
+
 
         public const SQLite.SQLiteOpenFlags Flags =
             // open the database in read/write mode
