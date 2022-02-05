e8e091b04bb68d876976f7749bf94f0f1824a6bd
Kruta-Prabhu <krutaprabhu96@gmail.com>
2022-02-05 12:14:48 -0800

    HomeMenuItem to HomeMenuItemTests.cs

---
+++ UnitTests/Models/HomeMnueItemTests.cs
@@ -0,0 +1,52 @@
+﻿using Mine.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests.Models
+{
+    [TestFixture]
+    public class HomeMnueItemTests
+    {
+        /// <summary>
+        /// Checking that ItemModel is not null
+        /// </summary>
+        [Test]
+        public void HomeMenuItem_Constructor_Valid_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new HomeMenuItem();
+
+            // Reset
+
+            // Assert 
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void HomeMenuItem_Set_Get_Valid_Default_Should_Pass()
+        {
+            // Arrange
+           
+
+            // Act
+            var result = new HomeMenuItem();
+            result.Id = MenuItemType.Items;
+            result.Title = "Title";
+
+            // Reset
+
+            // Assert 
+            Assert.IsNotNull(result);
+            
+            Assert.AreEqual(MenuItemType.Items, result.Id);
+            Assert.AreEqual("Title", result.Title);
+            
+        }
+
+
+    }
+}
