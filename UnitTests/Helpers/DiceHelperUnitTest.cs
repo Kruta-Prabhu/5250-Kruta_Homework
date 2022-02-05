4b0c40fa6a20e7e94fb7923ba45e951ab0730016
Kruta-Prabhu <krutaprabhu96@gmail.com>
2022-02-05 14:23:39 -0800

    Added Unit test RollDice_Valid_Roll_3_Dice_10_Fixed_5_should_return_5 to DiceHelperUnitTest.cs

--- UnitTests/Helpers/DiceHelperUnitTest.cs
+++ UnitTests/Helpers/DiceHelperUnitTest.cs
@@ -139,6 +139,24 @@ namespace UnitTests.Helpers
 
         }
 
+        /// <summary>
+        /// Test for RollDice(3,10) and fixed value of 5
+        /// </summary>
+        [Test]
+        public void RollDice_Valid_Roll_3_Dice_10_Fixed_5_Should_Return_15()
+        {
+            //Arrange
+            DiceHelper.ForceRollsToNotRandom = true;
+            DiceHelper.ForcedRandomValue = 5;
 
+            //Act
+            var result = DiceHelper.RollDice(3,10);
+
+            //Reset
+            DiceHelper.ForceRollsToNotRandom = false;
+
+            //Assert
+            Assert.AreEqual(15, result);
+        }
     }
 }
