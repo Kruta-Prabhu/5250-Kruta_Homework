7f956ef4dc5c897907f875af2c5ddb7567853f94
Kruta-Prabhu <krutaprabhu96@gmail.com>
2022-02-05 14:08:34 -0800

    Added  unit RollDice_InValid_Roll_0_Dice_10_Should_Return_Zero in DiceHelperUnitTest.cs

--- UnitTests/Helpers/DiceHelperUnitTest.cs
+++ UnitTests/Helpers/DiceHelperUnitTest.cs
@@ -83,6 +83,19 @@ namespace UnitTests.Helpers
 
         }
 
+        [Test]
+        public void RollDice_InValid_Roll_0_Dice_10_Should_Return_Zero()
+        {
+            //Arrange
+
+            //Act
+            var result = DiceHelper.RollDice(0,10);
+
+            //Reset
+
+            //Assert
+            Assert.AreEqual(true, result == 0);
+        }
 
     }
 }
