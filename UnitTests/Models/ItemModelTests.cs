0bd08acb6bd54e8b67a2dd967866d340945ed02b
Kruta-Prabhu <krutaprabhu96@gmail.com>
2022-02-05 09:52:52 -0800

    Updated UT to use new ItemModel in ItemModelTests.cs

--- UnitTests/Models/ItemModelTests.cs
+++ UnitTests/Models/ItemModelTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using Mine.Models;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,17 +9,21 @@ namespace UnitTests.Models
     [TestFixture]
     public class ItemModelTests
     {
+        /// <summary>
+        /// Checking that ItemModel is not null
+        /// </summary>
         [Test]
         public void ItemModel_Constructor_Valid_Default_Should_Pass()
         {
             // Arrange
 
             // Act
+            var result = new ItemModel();
 
             // Reset
 
             // Assert 
-            Assert.Pass();
+            Assert.IsNotNull(result);
         }
     }
 }
